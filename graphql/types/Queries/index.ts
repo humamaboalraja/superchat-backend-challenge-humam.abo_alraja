85578a9273adc8a5091a1b437f9189bcf825c5c1
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-11-24 01:38:13 +0100

    fix: Git's case sensitive names

--- graphql/types/Queries/index.ts
+++ graphql/types/Queries/index.ts
@@ -1,2 +1,2 @@
-export * from './Contact';
+export * from './contact';
 // export * from './Message';
---
+++ graphql/types/resourcesTypes/message/index.ts
@@ -0,0 +1,13 @@
+import { objectType } from 'nexus';
+
+export const Message = objectType({
+  name: 'Message',
+  definition(t) {
+    t.string('id');
+    t.string('messageContent');
+    t.string('senderId');
+    t.string('recieverId');
+    t.string('createdAt');
+    t.string('updatedAt');
+  },
+});
